fb8ccbf1cb5aa053a297a8b4dc68f5e034420de1
puuertA <81367824+puuertA@users.noreply.github.com>
2022-09-05 08:10:37 -0300

    Exercicio 4 atualizado e funcionando 100%

--- Lista 02 - Lista Linear Estática/3.c
+++ Lista 02 - Lista Linear Estática/3.c
@@ -1,1 +1,154 @@
+/*
+3. Desenvolva um programa em Linguagem C
+que permita fazer as seguintes
+operações sobre uma lista linear
+estática de números reais:
+inserir um elemento mantendo a ordem crescente entre os elementos no vetor;
+remover um valor indicado pelo usuário, eliminando eventuais "lacunas abertas" pela remoção.
+*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#define T 10
+
+typedef struct sLista
+{
+    int valor[T];
+    int n;
+} LISTA;
+
+int inicializar(LISTA *L)
+{
+    L->n = -1;
+}
+
+int cheia(LISTA *L)
+{
+    if(L->n+1 == T)
+        return 1;
+    else
+        return 0;
+}
+
+int vazia(LISTA *L)
+{
+    if(L->n == -1)
+        return 1;
+    else
+        return 0;
+}
+
+int inserirOrdenadamente(LISTA *L, int elem)
+{
+    if(cheia(L))
+    {
+        printf("\nERRO: lista cheia");
+    }
+    if(vazia(L)){ // ve se ta vazia
+        L->n = L->n+1; // aumenta o indice em 1 pra poder inserir
+        L->valor[L->n] = elem; // insere o elemento na posição do indice (fim da lista)
+        return 1; // ja sai fora se tiver vazia
+    }
+    int i=(L->n); // determina o i como o indice (fim da lista)
+    int p=(L->n)+1; // determina o p como uma posição a frente do indice (fim da lista  + 1)
+    while(elem<L->valor[i]){ // praticamente um if vendo se o o valor que quer inserir é maior que os da lista
+        L->valor[i+1] = L->valor[i]; // faz o valor da frente virar o que tem atras
+        i--; // diminui uma posição do i com intenção de achar a posição anterior ao valor inserido
+        p--; // diminui uma do p com intenção de achar a posição que vai ser devidamente inserido
+    }
+    L->valor[p] = elem; // insere na posição correta
+    L->n = L->n+1; // aumenta o indice em 1 para poder inserir
+    return 1; // so sai fora depois que reoordenar toda a lista
+}
+
+int removerElemento(LISTA *L, int elem)
+{
+
+}
+
+int imprimir(LISTA *L)
+{
+    int i;
+
+    if(vazia(L)) // verifica se tem alguma coisa pra imprimir
+    {
+        printf("\nERRO: lista vazia!");
+        return 1; // retorna 1 para tratar erros no main
+    }
+    else
+    {
+        for(i = 0; i<(L->n+1); i++) // enquanto não chegar no final da lista vai imprimindo
+        {
+            printf("\n%d - |%d|", i, L->valor[i]);
+        }
+    }
+}
+
+int menu()
+{
+    LISTA L; // declara variavel do tipo lista
+    inicializar(&L);
+    int op;
+
+    do
+    {
+        printf("\n+-------------------------------+");
+        printf("\n|          MENU LISTA           |");
+        printf("\n+-------------------------------+");
+        printf("\n|1 - Inserir val. ordenadamente |");
+        printf("\n|2 - Remover val. especifico    |");
+        printf("\n|3 - Exibir os elementos        |");
+        printf("\n+-------------------------------+");
+
+        printf("\n\nEscolha uma opcao\n>>> ");
+        scanf("%d", &op);
+
+        switch(op)
+        {
+        case 1:
+            ;
+            int elem = 0;
+
+            printf("\n\nIndique o elemento que sera inserido\n>>> ");
+            scanf("%d", &elem);
+
+            inserirOrdenadamente(&L,elem);
+
+            printf("\n");
+            system("pause");
+            system("clear||cls");
+            break;
+
+        case 2:
+            ;
+//            printf("\n\nIndique o elemento sera removido\n>>> ");
+//            scanf("%d", &elem);
+//
+//            removerPosicaoEspecifica(&L, elem);
+//
+//            printf("\n");
+//            system("pause");
+//            system("clear||cls");
+            break;
+
+        case 3:
+            ;
+            printf("\nExibindo os elementos da lista: \n\n");
+            imprimir(&L);
+
+            printf("\n");
+            system("pause");
+            system("clear||cls");
+            break;
+        default:
+            ;
+            printf("\nInsira uma opcao valida");
+        }
+    }
+    while(op!=0);
+}
+
+int main()
+{
+    menu();
+}
--- Lista 02 - Lista Linear Estática/4.c
+++ Lista 02 - Lista Linear Estática/4.c
@@ -1,99 +1,123 @@
-/* 4. Desenvolva um programa em Linguagem C que possua uma função que, dada duas
-listas, L1 e L2, concatene L1 com L2 armazenando o resultado em L1. */
+/*
+4. Desenvolva um programa em Linguagem C que possua uma função que,
+dada duas listas, L1 e L2, concatene L1 com L2 armazenando o resultado em L1.
+*/
+
 #include <stdio.h>
-#define T 5
+#include <stdlib.h>
+#define T 10
 
 typedef struct sLista
 {
-    int vet[T];
+    int valor[T];
     int n;
 } LISTA;
 
-void inicializar(LISTA *lis)
+int inicializar (LISTA *L)
 {
-    (*lis).n = -2;
-    lis->n = -1; // na função utiliza-se o ponteiro como "->"
+    L->n = -1;
 }
 
-int cheia(LISTA lista)
+int vazia(LISTA *L)
 {
-    if((*lista).n == T - 1)
-        return 1; // pode ser usado para validar como cheio
-    else
-        return 0; // pode ser usado para validar como vazio
-}
-
-int vazia(LISTA lista){
-    if((*lista).n == -1)
+    if(L->n == -1)
         return 1;
     else
         return 0;
 }
 
-void inserirElementoFim(LISTA *lista, int valor)
+int cheia(LISTA *L)
 {
-    if(cheia(&lista))
+    if(L->n+1 == T)
+        return 1;
+    else
+        return 0;
+}
+
+int inserirNoFim(LISTA *L, int elem)
+{
+    if(cheia(L)) // verifica se tem espaço na lista para inserir
     {
-        printf("Lista cheia!!! Ferrou...");
+        printf("\nERRO: lista cheia!");
+        return 1; // retorna 1 para tratar erros no main
     }
     else
     {
-        lista->n++;
-        lista->vetor[lista->n] = valor;
+        L->valor[L->n+1] = elem; // como o indice "n" é iniciado na posição "-1" para se inserir passasse para o valor a posição n+1, que no caso vai ser a primeira posição na frente do indice;
+        L-> n = L-> n+1; // atualiza a posição do indice, pois inseriu-se um elemento na posição em que "n" estava
+        return 0; // retorna 0 para tratar erros no main
     }
 }
 
-void exibirLista(LISTA lista)
+int imprimir(LISTA *L)
 {
     int i;
-    if(vazia(&lista))
+
+    if(vazia(L)) // verifica se tem alguma coisa pra imprimir
     {
-        printf("Lista vazia!!!");
+        printf("\nERRO: lista vazia!");
+        return 1; // retorna 1 para tratar erros no main
     }
     else
     {
-        for(i = 0; i <= lista.n; i++)
+        for(i = 0; i<(L->n+1); i++) // enquanto não chegar no final da lista vai imprimindo
         {
-            printf("%d ", lista.vetor[i]);
+            printf("\n%d - |%d|", i, L->valor[i]);
         }
     }
 }
 
-void concatenarListas(LISTA *lista1, LISTA lista2){
-    int i;
-    if((lista1->n + 1) + (lista2.n + 1) <= TAM)
+int concatenarListas(LISTA *L1, LISTA *L2)
+{
+    if((L1->n) + (L2->n) <= T)
     {
-        for(i = 0; )
-    }else{
-        printf("Tamanho insuficiente");
+        for(int i=0; i<L2->n; i++)
+        {
+            inserirNoFim(L1, L2->valor[i]);
+        }
+    }
+    else
+    {
+        printf("\nERRO: Tamanho insuficiente");
     }
 }
 
+
 void main()
 {
-    LISTA lista1, lista2;
-    inicializar(&lista1);
-    inicializar(&lista2);
+    LISTA L1, L2;
+    inicializar(&L1);
+    inicializar(&L2);
     int valor;
-
-//    printf("%d",lista.n); // no main utiliza-se o ponteiro como "."
+    int i = 1;
+    int j = 1;
+    do
+    {
+        printf("\nIndique o elemento %d a ser inserido em L1 (0 para sair)\n>>> ", i);
+        scanf("%d", &valor);
+        if(valor != 0)
+            inserirNoFim(&L1,valor);
+        i++;
+    }
+    while(valor != 0);
+    imprimir(&L1);
 
     do
     {
-        printf("Digite o valor para a lista 1 (0 - sair):");
+        printf("\nIndique o elemento %d a ser inserido em L2 (0 para sair)\n>>> ", j);
         scanf("%d", &valor);
         if(valor != 0)
-            inserirElementoFim(&lista1 ,valor);
-    }while(valor != 0);
+            inserirNoFim(&L2,valor);
+        j++;
+    }
+    while(valor != 0);
+    imprimir(&L2);
 
-    exibirLista(lista1);
-    do{
-        printf("\nDigite o valor para a lista 2 (0 - sair)");
-        scanf("%d", &valor);
-        if(valor != 0)
-            inserirElementoFim(&lista2, valor);
-    }while (valor != 0);
-    exibirLista(lista2);
+    printf("\nConcatenando listas...\n");
+    concatenarListas(&L1, &L2);
+    system("pause");
 
-    concatenarListas(&lista1, &lista2);
+    printf("\nListas concatenadas em L1");
+    imprimir(&L1);
 }
+
